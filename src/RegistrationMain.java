46e218299351d055f68d2edaf1244b7c6cc84274
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-16 22:45:25 +0530

    userRegistration uc2  is added

--- src/RegistrationMain.java
+++ src/RegistrationMain.java
@@ -7,14 +7,21 @@ public class RegistrationMain {
 		Scanner scanner = new Scanner(System.in);
 		UserRegistration register = new UserRegistration();
 
-		System.out.println("Enter first name");
+		System.out.println("Enter Firstname which starts with Cap and has minimum 3 characters");
 		String firstName = scanner.nextLine();
-
 		Boolean firstNameCheck = register.validateFirstName(firstName);
 		if (firstNameCheck) {
 			System.out.println("Valid first name");
 		} else
 			System.out.println("Invalid first name");
-	}
 
+		System.out.println("\nEnter Secondname which starts with Cap and has minimum 3 characters");
+		String secondName = scanner.nextLine();
+
+		Boolean secondNameCheck = register.validateSecondName(secondName);
+		if (secondNameCheck) {
+			System.out.println("Valid second name");
+		} else
+			System.out.println("Invalid second name");
+	}
 }
--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -5,13 +5,25 @@ public class UserRegistration {
 	/**
 	 * method to validate first name
 	 * 
-	 * @param firstName -first name is passed as paramter
+	 * @param firstName -first name is passed as parameter
 	 * @return
 	 */
 	public Boolean validateFirstName(String firstName) {
-		String matcher = "^[A-Z][a-z]{2}[a-z]*";
-		Boolean check = Pattern.matches(matcher, firstName);
-		return check;
+		String string = "^[A-Z][a-z]{2}[a-z]*";
+		Boolean result = Pattern.matches(string, firstName);
+		return result;
+	}
+
+	/**
+	 * method to validate second name
+	 * 
+	 * @param secondName -second name is passed as parameter
+	 * @return
+	 */
+	public Boolean validateSecondName(String secondName) {
+		String string = "^[A-Z][a-z]{2}[a-z]*";
+		Boolean result = Pattern.matches(string, secondName);
+		return result;
 	}
 
 }
